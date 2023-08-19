0878353dcf3b50b4c86799d57265e9e39ffdbf1d
priyanka-111-droid <77121296+priyanka-111-droid@users.noreply.github.com>
2023-08-19 20:10:47 +0530

    added problem of the day

---
+++ src/Codestudio_ProblemOfTheDay/Aug19_Easy.java
@@ -0,0 +1,14 @@
+import java.util.Arrays;
+public class Aug19_Easy {
+
+    static int minDiff(int n, int[] arr) {
+        Arrays.sort(arr);
+        int mini = Integer.MAX_VALUE;
+
+        for(int i=1;i<n;i++){
+            //    int diff = Math.abs(arr[i]-arr[i-1]);
+            mini = Math.min(mini,Math.abs(arr[i]-arr[i-1]));
+        }
+        return mini;
+    }
+}
